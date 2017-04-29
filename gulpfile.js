46a499ffcda4086e11f6115e6c2f6479003c02f6
Michael Casey <mcasey8540@gmail.com>
2017-04-29 07:13:43 -0400

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var webserver = require('gulp-webserver');
+var rename = require('gulp-rename');
+var minifyCss = require('gulp-minify-css');
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// launches a web server that serves files in the current directory
+//
+/////////////////////////////////////////////////////////////////////////////////////
+
+gulp.task('webserver', function() {
+    gulp.src('.')
+      .pipe(webserver({
+          port: 8001,
+          livereload: true,
+          directoryListing: false,
+          open: "http://localhost:8001/index.html"
+      }));
+});
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// launch a dev webserver and watch for scss changes
+//
+/////////////////////////////////////////////////////////////////////////////////////
+
+gulp.task('dev', gulp.parallel( 'webserver'));
+
+
