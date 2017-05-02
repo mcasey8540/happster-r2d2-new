9924777eacdb02198cdb5ad81041867cb40c3f32
Michael Casey <mcasey8540@gmail.com>
2017-05-02 08:01:41 -0400

    Add sidebar and remove blockers

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,30 @@ var webserver = require('gulp-webserver');
 var rename = require('gulp-rename');
 var minifyCss = require('gulp-minify-css');
 
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// watches for scss changes
+//
+/////////////////////////////////////////////////////////////////////////////////////
+
+gulp.task('scss', function () {
+  return gulp.src('./assets/happster/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./assets/happster/css'))
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./assets/happster/css'))
+    //.on('end', done);
+});
+
+gulp.task('scss:watch', function () {
+  gulp.watch('./assets/happster/scss/**/*.scss', gulp.series(['scss']));
+});
+
+
 /////////////////////////////////////////////////////////////////////////////////////
 //
 // launches a web server that serves files in the current directory
@@ -27,6 +51,14 @@ gulp.task('webserver', function() {
 //
 /////////////////////////////////////////////////////////////////////////////////////
 
-gulp.task('dev', gulp.parallel( 'webserver'));
+gulp.task('dev', gulp.parallel('scss','scss:watch','webserver'));
+
+
+
+
+
+
+
+
 
 
